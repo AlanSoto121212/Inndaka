def246d207ef0382ea9d330b75502d9c44f6ff1e
Alan <168918140+AlanSoto121212@users.noreply.github.com>
2024-05-04 12:03:34 -0600

    Files upload

---
+++ admin/script.js
@@ -0,0 +1,58 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 4000); // Cambia la imagen cada 3000 milisegundos (3 segundos)
+}
+
+// Inicia el carrusel
+showSlides();
+
+
+
+//PARA COLABORADORES
+document.getElementById('colaboradores-container').addEventListener('click', function() {
+    // Redirigir al otro index (ajusta la URL según tu necesidad)
+    window.location.href = 'modulos/Colaboradores/index.php'; /*AKI PONES LA DIRECCION ADRIAN */
+});
+
+
+//PARA EL RUM
+document.getElementById('rum-container').addEventListener('click', function() {
+    // Redirigir al otro index (ajusta la URL según tu necesidad)
+    window.location.href = '../rum/index.php';  /*AKI PONES LA DIRECCION ADRIAN */
+});
+
+
+//PARA EL RUM monitorista
+document.getElementById('monitorista-container').addEventListener('click', function() {
+    // Redirigir al otro index (ajusta la URL según tu necesidad)
+    window.location.href = '../rum_mon/index.php';  /*AKI PONES LA DIRECCION ADRIAN */
+});
+
+
+
+//PARA LA GACETA
+document.getElementById('gaceta-container').addEventListener('click', function() {
+    // Redirigir al otro index (ajusta la URL según tu necesidad)
+    window.location.href = 'modulos/GACETA/index.php';  /*AKI PONES LA DIRECCION ADRIAN */
+});
+
+//PARA EL INVENTARIO
+document.getElementById('inventario-container').addEventListener('click', function() {
+    // Redirigir al otro index (ajusta la URL según tu necesidad)
+    window.location.href = 'modulos/Inventario/index.php';  /*AKI PONES LA DIRECCION ADRIAN */
+});
